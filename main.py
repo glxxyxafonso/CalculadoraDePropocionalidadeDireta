4ae3372d392d65078c10e40d21e0e87f91607d18
Afonso Beato <96067139+glxxyxafonso@users.noreply.github.com>
2024-04-30 11:09:39 +0100

    Version 1.2 Upload

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Version 1.1
+# Version 1.2
 # Code available in English at https://github.com/glxxyxafonso/
 
 # Dear programmer,
@@ -7,7 +7,7 @@
 # If you are trying to optimise this code (that you probably won't be able to)
 # Please add how much time you spent on this in this counter:
 
-# Hours_Spent_On_This_Code = 1
+# Hours_Spent_On_This_Code = 2
 
 from time import sleep as wait
 
@@ -29,11 +29,13 @@ print()
 numtworesult = float(numtwotwo) / float(numtwo) # Repeti a mesma coidsa que na linha 9. 
 
 if numtworesult == numoneresult:
-  print("No momento, é uma proporcionalidade direta.")
+  print("No momento, é uma propocionalidade direta.")
   print()
   print("Vamos continuar.")
+  print("Se deseja acabar, faça CTRL+C")
+  wait(3)
 else: 
-  print("Não é uma proporcionalidade direta.")
+  print("Não é uma propocionalidade direta.")
   print("Obrigado por teres usado esta calculadora!")
   quit()
 
@@ -43,10 +45,117 @@ numthreetwo = input("Insira o terceiro f(X): ")
 numthreeresult = float(numthreetwo) / float(numthree)
 
 if numthreeresult == numoneresult:
-  print("No momento, é uma proporcionalidade direta.")
+  print("No momento, é uma propocionalidade direta.")
   print()
   print("Vamos continuar.")
+  print("Se deseja acabar, faça CTRL+C")
+  wait(3)
 else:
-  print("Não é uma proporcionalidade direta.")
+  print("Não é uma propocionalidade direta.")
   print("Obrigado por teres usado esta calculadora!")
   quit()
+
+numfour = input("Insira o quarto X: ")
+print()
+numfourtwo = input("Insira o quarto f(X): ")
+numfourresult = float(numfourtwo) / float(numfour)
+
+if numfourresult == numoneresult:
+  print("No momento, é uma propocionalidade direta.")
+  print()
+  print("Se deseja acabar, faça CTRL+C")
+  wait(3)
+else:
+  print("Não é uma propocionalidade direta.")
+  print("Obrigado por teres usado esta calculadora!")
+  quit()
+
+numfive = input("Insira o quinto X: ")
+print()
+numfivetwo = input("Insira o quinto f(X): ")
+numfiveresult = float(numfivetwo) / float(numfive)
+
+if numfiveresult == numoneresult:
+  print("No momento, é uma propocionalidade direta.")
+  print()
+  print("Se deseja acabar, faça CTRL+C")
+  wait(3)
+else:
+  print("Não é uma propocionalidade direta.")
+  print("Obrigado por teres usado esta calculadora!")
+  quit()
+
+numsix = input("Insira o sexto X: ")
+print()
+numsixtwo = input("Insira o sexto f(X): ")
+numsixresult = float(numsixtwo) / float(numsix)
+
+if numsixresult == numoneresult:
+  print("No momento, é uma propocionalidade direta.")
+  print()
+  print("Se deseja acabar, faça CTRL+C")
+  wait(3)
+else:
+  print("Não é uma propocionalidade direta.")
+  print("Obrigado por teres usado esta calculadora!")
+  quit()
+
+numseven = input("Insira o sétimo X: ")
+print()
+numseventwo = input("Insira o sétimo f(X): ")
+numsevenresult = float(numseventwo) / float(numseven)
+
+if numoneresult == numsevenresult:
+  print("No momento, é uma propocionalidade direta.")
+  print()
+  print("Se deseja acabar, faça CTRL+C")
+  wait(3)
+else:
+  print("Não é uma propocionalidade direta.")
+  print("Obrigado por teres usado esta calculadora!")
+  quit()
+
+numeight = input("Insira o oitavo X: ")
+print()
+numeighttwo = input("Insira o oitavo f(X): ")
+numeightresult = float(numeighttwo) / float(numeight)
+
+if numeightresult == numoneresult:
+  print("No momento, é uma propocionalidade direta.")
+  print()
+  print("Se deseja acabar, faça CTRL+C")
+  wait(3)
+else:
+  print("Não é uma propocionalidade direta.")
+  print("Obrigado por teres usado esta calculadora!")
+  quit()
+
+numnine = input("Insira o nono X: ")
+print()
+numninetwo = input("Insira o nono f(X): ")
+numnineresult = float(numninetwo) / float(numnine)
+
+if numnineresult == numoneresult:
+  print("No momento, é uma propocionalidade direta.")
+  print()
+  print("Se deseja acabar, faça CTRL+C")
+  wait(3)
+else:
+  print("Não é uma propocionalidade direta.")
+  print("Obrigado por teres usado esta calculadora!")
+  quit()
+
+numten = input("Insira o décimo X: ")
+print()
+numtentwo = input("Insira o décimo f(X): ")
+numtenresult = float(numtentwo) / float(numten)
+
+if numtenresult == numoneresult:
+  print("É uma propocionalidade direta.")
+  print()
+  print("O utilizador chegou ao limite de X e f(X) que pode inserir. Obrigado por ter usado a calculadora!")
+  wait(4)
+else:
+  print("Não é uma propocionalidade direta.")
+  print("Obrigado por teres usado esta calculadora!")
+  quit()x
